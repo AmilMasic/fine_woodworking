e528a5b5162a0a4c314d5add25e6b9c31f38904e
Amil Masic <amil.masic@gmail.com>
2019-02-26 13:42:48 +0000

    add fake CLI structure

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,13 +3,44 @@ class FineWoodworking::CLI
   def call
     puts "Today's Daily Deals:"
     list_articles
+    menu
+    goodbye
   end
 
   def list_articles
-    puts <<-DOC
+    puts <<-DOC.gsub /^\s+/, ""
     1 . Article 1
     2. Article 2
+    3. Article 3
+    4. Article 4
     DOC
-
   end
+
+  def menu
+
+    input = nil
+    while input != "exit"
+      puts "Enter the number of which article you want to read. Type list if you want to see the list of articles again. Type exit if you would like to quit the program."
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "more info on article 1.."
+      when "2"
+        puts "more info on article 2.."
+      when "3"
+        puts "more info on article 3.."
+      when "4"
+        puts "more info on article 4.."
+      when "list"
+        list_articles
+      else
+        puts "Input error, please select number 1 - 4, list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "See you next time for new articles"
+  end
+
 end
