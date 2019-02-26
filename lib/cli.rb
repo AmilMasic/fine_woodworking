c22103210802179fc2b9b688903cbbb5260fc751
Amil Masic <amil.masic@gmail.com>
2019-02-26 13:28:38 +0000

    HEREDOC bug fix

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,14 @@ class FineWoodworking::CLI
 
   def call
     puts "Today's Daily Deals:"
+    list_articles
   end
 
+  def list_articles
+    puts <<-DOC
+    1 . Article 1
+    2. Article 2
+    DOC
+
+  end
 end
