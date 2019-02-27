93670b17ddccc81cb7abd238664a783f7f18e96f
Amil Masic <amil.masic@gmail.com>
2019-02-27 11:19:20 +0000

    fix displaying articles

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,21 +1,19 @@
 class FineWoodworking::CLI
 
   def call
-    puts "Today's Daily Deals:"
+
     list_articles
     menu
     goodbye
   end
 
   def list_articles
-    puts <<-DOC.gsub /^\s+/, ""
-    1 . Article 1
-    2. Article 2
-    3. Article 3
-    4. Article 4
-    DOC
-
+    puts "Today's Articles on the front page of FineWoodworking."
     @articles = FineWoodworking::Article.today
+    @articles.each.with_index(1) do |article, i|
+      puts "#{i}. #{article.title} - #{article.category}. - #{article.teaser}"
+      
+    end
   end
 
   def menu
@@ -24,25 +22,12 @@ class FineWoodworking::CLI
         puts "Enter the number of which article you want to read. Type list if you want to see the list of articles again. Type exit if you would like to quit the program."
         input = gets.strip.downcase
         if input.to_i > 0
-          puts @articles[input.to_i-1]
+          article = @articles[input.to_i-1]
+          puts "#{article.title} - #{article.category}"
         elsif input == "list"
           list_articles
         else
           puts "Input error, please select number 1 - 4, list or exit."
-
-        # case input
-        # when "1"
-        #   puts "more info on article 1.."
-        # when "2"
-        #   puts "more info on article 2.."
-        # when "3"
-        #   puts "more info on article 3.."
-        # when "4"
-        #   puts "more info on article 4.."
-        # when "list"
-        #   list_articles
-        # else
-        #   puts "Input error, please select number 1 - 4, list or exit."
         end
       end
     end
